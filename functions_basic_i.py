e6115bdf088bc8d0f128ff9a73f017c181f89f23
HenryCodeT <120686@unsaac.edu.pe>
2022-01-17 17:47:55 -0500

    first commit

---
+++ functions_basic_i.py
@@ -0,0 +1,157 @@
+#1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+#>>> 5
+
+# #2
+# def number_of_military_branches():
+    # return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#>>> number_of_days_in_a_week_silicon_or_triangle_sides error is not defined
+
+# #3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+#>>> 5
+
+# #4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+# >>> 5
+
+# #5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+#>>>5
+#>>>none
+
+# #6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+#>>>error nonetype
+
+# #7
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+#>>>25
+
+# #8
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+#>>>100
+#>>>10
+
+# #9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    # if b<c:
+        # return 7
+    # else:
+        # return 14
+    # return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#b=2
+#c=3
+#Primer print>>> 7
+#b=5
+#c=3
+#Segundo print>>> 14
+#Tercer print>>> 21
+
+# #10
+# def addition(b,c):
+    # return b+c
+    # return 10
+# print(addition(3,5))
+#>>> 8
+
+# #11
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+#ln-87-print>>>500
+#ln-91-print>>>500
+#ln-90-print>>>300
+#ln-93-print>>>500
+
+# #12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+#ln-101-print>>>500
+#ln-106-print>>>500
+#ln-104-print>>>300
+#ln-108-print>>>500
+
+# #13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+#ln-116-print>>>500
+#ln-121-print>>>500
+#ln-119-print>>>300
+#ln-123-print>>>300
+
+# #14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+#ln-131-print>>>1
+#ln-135-print>>>3
+#ln-133-print>>>2
+
+# #15
+# def foo():
+#     print(1)
+#     x = bar()
+#     print(x)
+#     return 10
+# def bar():
+#     print(3)
+#     return 5
+# y = foo()
+# print(y)
+#ln-143-print>>>1 
+#ln-148-print>>>3
+#ln-145-print>>>5
+#ln-151-print>>>10
+
+ 
